cb34a7954dc340878c419783d51572a7336a61c0
ravas <z.kinshef@gmail.com>
2020-04-21 11:52:52 +0300

    feat: all file.

---
+++ plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: gutenberg-calculator
+ * Version: 1.0.0
+ *
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Block Initializer.
+ */
+require_once plugin_dir_path( __FILE__ ) . 'src/init.php';
